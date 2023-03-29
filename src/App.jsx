08a6be84af092ea23d168987d7e64bb478688294
Unaiza898 <unaiza.nizami@cix.csi.cuny.edu>
2023-03-28 21:25:35 -0400

    commit

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import './App.css'
+import { useState, useEffect } from "react";
+// import CoinInfo from './Components/CoinInfo';
+import CoinInfo from "./Components/coinInfo";
+export default function App() {
+
+  const API_KEY = "3eb132ce569ebbf66ab36f1ec1a3ec497afe367449cba6794dc6cc5718b64cfd";
+  const [list, setList] = useState(null);
+  const [filteredResults, setFilteredResults] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+  useEffect(() => {
+    const fetchAllCoinData = async () => {
+      const response = await fetch(
+        "https://min-api.cryptocompare.com/data/all/coinlist?&api_key"
+        + API_KEY
+        // how do we call an API using fetch? 
+      );
+
+      const json = await response.json();
+      setList(json);
+
+      console.log(list)
+
+    };
+
+    fetchAllCoinData().catch(console.error);
+  }, []);
+
+  const searchItems = searchValue => {
+    setSearchInput(searchValue);
+    if (searchValue !== "") {
+      const filteredData = Object.keys(list.Data).filter((item) => 
+        Object.values(item)
+          .join("")
+          .toLowerCase()
+          .includes(searchValue.toLowerCase())
+      )
+      setFilteredResults(filteredData);
+    } else {
+      setFilteredResults(Object.keys(list.Data));
+    }
+  };
+  return (
+    <div className="whole-page">
+      <h1>My Crypto List</h1>
+      <input
+    type="text"
+    placeholder="Search..."
+    onChange={(inputString) => searchItems(inputString.target.value)}
+  />
+      <ul>
+        {console.log(list)}
+        {list && Object.entries(list.Data).map(([coin]) =>
+          list.Data[coin].PlatformType === "blockchain" ? (
+            <li key={list.Data[coin].FullName}>{list.Data[coin].FullName}</li>
+          ) : null
+        )}
+      </ul>
+      
+    </div>
+  )
+}
